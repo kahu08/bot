ca58ad4c8b6f3bd03c626bb6545eb043db53ac8e
kahu08 <peterkahurani@gmail.com>
2017-10-23 11:46:24 +0300

    added sql statement

---
+++ db.js
@@ -0,0 +1,24 @@
+connection.connect();
+
+module.exports = connection;
+
+// const products = [{
+//     name: 'Tomato Sauce',
+//     location: 500,
+//     description: 'Sweet and thick',
+//     photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
+//   },
+//   {
+//     name: 'Chilli Sauce',
+//     location: 270,
+//     description: 'hot and good!',
+//     photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
+//
+//   },
+//   {
+//     name: 'Sour Sauce',
+//     location: 300,
+//     description: 'Sweet and sour',
+//     photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
+//   }
+// ]
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 'use strict'
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: ''
+});
+
+// start connection to the database
+connection.connect();
+
+var products;
+connection.query('select a.name, b.amount from events a join event_tickets b on a.id = b.event_id', function(error, results, fields) {
+  if (error) throw error;
+  //  console.log('The solution is: ', results[0].solution);
+  products = results;
+});
+
+
+connection.end();
+
 const Telegraf = require('telegraf')
 const {
   Markup
@@ -8,30 +29,37 @@ const {
 const app = new Telegraf('')
 // const PAYMENT_TOKEN = ''
 
-const products = [{
-    name: 'Tomato Sauce',
-    price: 500,
-    description: 'Sweet and thick',
-    photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
-  },
-  {
-    name: 'Chilli Sauce',
-    price: 500,
-    description: 'hot and good!',
-    photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
+// const products = [{
+//     name: 'Tomato Sauce',
+//     location: 500,
+//     description: 'Sweet and thick',
+//     photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
+//   },
+//   {
+//     name: 'Chilli Sauce',
+//     location: 270,
+//     description: 'hot and good!',
+//     photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
+//
+//   },
+//   {
+//     name: 'Sour Sauce',
+//     location: 300,
+//     description: 'Sweet and sour',
+//     photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
+//   }
+// ]
 
-  },
-  {
-    name: 'Sour Sauce',
-    price: 500,
-    description: 'Sweet and sour',
-    photoUrl: 'http://www.hunts.com/sites/g/files/qyyrlu211/files/images/products/no-salt-added-ketchup-40824.png'
 
-  }
-]
+app.command('start', ({
+  reply
+}) => reply('welcome to TicketSoko! Ask what we have to see the events:'))
+// regex to reply when user enters anything with what
+app.hears(/^what.*/i, ({
+  replyWithMarkdown
+}) => replyWithMarkdown(`Let me show you my products!
+  ${products.reduce((acc, p) => acc += `*${p.name}* - KES ${p.amount} \n`, '')}
+  What do You want?`, Markup.keyboard(products.map(p => p.name)).oneTime().resize().extra()));
 
-
-app.command('start', ({ reply }) => reply('welcome, i can sell you stuff just ask'))
-app.hears(/^what.*/i, ({ replyWithMarkdown }) => replyWithMarkdown('foo'));
 
 app.startPolling()
